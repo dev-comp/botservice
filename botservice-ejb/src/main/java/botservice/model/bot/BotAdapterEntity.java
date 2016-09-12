f09ff6f7a0e5470da7400361374a0538cb5aa4d3
d.soldatkin <d.soldatkin@bftcom.com>
2016-09-12 17:41:53 +0300

    Добавлена возможность сохранения файлов-адаптеров. Пока сохраняем куда попало (хардкод)

--- botservice-ejb/src/main/java/botservice/model/bot/BotAdapterEntity.java
+++ botservice-ejb/src/main/java/botservice/model/bot/BotAdapterEntity.java
@@ -18,11 +18,21 @@ public class BotAdapterEntity extends AbstractBaseEntity{
     @Size(min = 1, max = 256)
     private String name;
 
+    private String filePath;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
     }
 }
--- botservice-web/src/main/java/botservice/web/controller/bot/BotAdapterEditModel.java
+++ botservice-web/src/main/java/botservice/web/controller/bot/BotAdapterEditModel.java
@@ -8,9 +8,15 @@ import botservice.service.common.BaseParam;
 import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.servlet.http.Part;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Компонент-подложка для управления одним адаптером бота
@@ -28,6 +34,8 @@ public class BotAdapterEditModel implements Serializable {
 
     private boolean isNew = true;
 
+    private Part botAdapterPart;
+
     @PostConstruct
     public void init(){
         String idParam = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("id");
@@ -35,10 +43,6 @@ public class BotAdapterEditModel implements Serializable {
             loadBotAdapterEntity(Long.parseLong(idParam));
             isNew = false;
         }
-    }
-
-    public BotAdapterEntity getBotAdapterEntity() {
-        return botAdapterEntity;
     }
 
     private void loadBotAdapterEntity(Long botAdapterEntityId){
@@ -49,5 +53,38 @@ public class BotAdapterEditModel implements Serializable {
         botAdapterEntity = botService.mergeEntity(botAdapterEntity);
         if (isNew)
             loadBotAdapterEntity(botAdapterEntity.getId());
+    }
+
+    public void uploadBotAdapter(){
+        if (botAdapterPart != null) {
+            String header = botAdapterPart.getHeader("content-disposition");
+            String botAdapterFileName = null;
+            for(String headerPart : header.split(";")) {
+                if(headerPart.trim().startsWith("filename")) {
+                    botAdapterFileName = headerPart.substring(headerPart.indexOf('=') + 1).trim().replace("\"", "");
+                    break;
+                }
+            }
+            //todo определиться с местом сохранения адаптера
+            Path destination = Paths.get("D:/work/dev-comp/" + botAdapterFileName);
+            try {
+                Files.copy(botAdapterPart.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            botAdapterEntity.setFilePath(destination.toString());
+        }
+    }
+
+    public BotAdapterEntity getBotAdapterEntity() {
+        return botAdapterEntity;
+    }
+
+    public Part getBotAdapterPart() {
+        return botAdapterPart;
+    }
+
+    public void setBotAdapterPart(Part botAdapterPart) {
+        this.botAdapterPart = botAdapterPart;
     }
 }
