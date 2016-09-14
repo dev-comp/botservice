ea53f2e4cd16ee84814a9817b5769d9a72a01660
d.soldatkin <d.soldatkin@bftcom.com>
2016-09-14 21:17:20 +0300

    Добавил педали старт/стоп  в список ботов

--- botservice-web/src/main/java/botservice/web/controller/bot/botEntry/BotEntryListModel.java
+++ botservice-web/src/main/java/botservice/web/controller/bot/botEntry/BotEntryListModel.java
@@ -28,13 +28,43 @@ public class BotEntryListModel implements Serializable {
         botEntryList = botService.getEntityList(BotEntryEntity.class);
     }
 
+    private void doSaveBotEntryEntity (BotEntryEntity botEntryEntity){
+        BotEntryEntity updatedBotEntryEntity = botService.mergeEntity(botEntryEntity);
+        botEntryEntity.setVersion(updatedBotEntryEntity.getVersion());
+    }
+
+    public boolean isDeleteBotEntryDisabled(BotEntryEntity botEntryEntity){
+        return botEntryEntity.getState() == 1;
+    }
+
     public List<BotEntryEntity> getBotEntryList(){
         return botEntryList;
     }
 
-    public void doDeleteBotEntry(BotEntryEntity botAdapterEntity){
-        botEntryList.remove(botAdapterEntity);
-        botService.removeEntity(botAdapterEntity);
+    public void doDeleteBotEntry(BotEntryEntity botEntryEntity){
+        botEntryList.remove(botEntryEntity);
+        botService.removeEntity(botEntryEntity);
     }
+
+    public boolean isStartBotEntryDisabled(BotEntryEntity botEntryEntity) {
+        return botEntryEntity.getState() == 1;
+    }
+
+    public void doStartBotEntry(BotEntryEntity botEntryEntity){
+        botEntryEntity.setState(1);
+        doSaveBotEntryEntity(botEntryEntity);
+        //todo послать сообщение адаптеру о добавлении потока
+    }
+
+    public boolean isStopBotEntryDisabled(BotEntryEntity botEntryEntity){
+        return botEntryEntity.getState() == 0;
+    }
+
+    public void doStopBotEntry(BotEntryEntity botEntryEntity){
+        botEntryEntity.setState(0);
+        doSaveBotEntryEntity(botEntryEntity);
+        //todo послать сообщение адаптеру об остановке потока
+    }
+
 
 }
